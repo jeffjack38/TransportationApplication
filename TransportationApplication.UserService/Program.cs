9f014c91fda5bf0df6055a7a4058c526c9a4cfbd
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-11-07 09:49:51 -0800

    FirstMigrationSuccess for User-Seeded roles and Admin

--- TransportationApplication.UserService/Program.cs
+++ TransportationApplication.UserService/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Identity;
 using TransportationApplication.SharedModels;
+using Microsoft.Extensions.DependencyInjection;
+using TransportationApplication.UserService.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddIdentity<User, IdentityRole>();
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UserDbContext")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<UserDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +29,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Seed Admin Role and User on Startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+    await SeedAdminUserAsync(userManager, roleManager);
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -29,3 +46,41 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//method for seeding Admin user and role
+async Task SeedAdminUserAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+{
+    string adminRole = "Admin";
+    string adminEmail = "admin@example.com";
+    string adminPassword = "Admin@123";
+
+    //check if the admin role exists if not create it
+    if (!await roleManager.RoleExistsAsync(adminRole))
+    {
+        await roleManager.CreateAsync(new IdentityRole(adminRole));
+    }
+
+    //check if admin user already exists
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+    if (adminUser == null)
+    {
+        //create the Admin user
+        adminUser = new User
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            EmailConfirmed = true,
+            FullName = "AdminUser"
+        };
+
+        //create the user with the specified password
+        var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+        //if success, assing Admin role to user
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(adminUser, adminRole);
+        }
+    }
+}
